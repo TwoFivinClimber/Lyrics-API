ed0603498ae8d7e2f16b3a7d8bba929b318fd80a
Brett Hughes <twofivinclimber@gmail.com>
2022-05-17 22:06:55 -0500

    code created, not functioning

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,25 +1,51 @@
 // USE WITH FIREBASE AUTH
 // import checkLoginStatus from './helpers/auth';
+import axios from 'axios';
 import 'bootstrap'; // import bootstrap elements and js
 import '../styles/main.scss';
 
-const init = () => {
-  document.querySelector('#app').innerHTML = `
-    <h1>HELLO! You are up and running!</h1>
-    <small>Open your dev tools</small><br />
-    <button class="btn btn-danger" id="click-me">Click ME!</button><br />
-    <hr />
-    <h2>These are font awesome icons:</h2>
-    <i class="fas fa-user fa-4x"></i> <i class="fab fa-github-square fa-5x"></i>
-  `;
-  console.warn('YOU ARE UP AND RUNNING!');
+// API CALL
+const getLyrics = (artist, song) => ((resolve, reject) => {
+  axios.get(`https://api.lyrics.ovh/v1/${artist}/${song}`)
+    .then((response) => resolve(response.data))
+    .catch((error) => reject(error));
+});
 
-  document
-    .querySelector('#click-me')
-    .addEventListener('click', () => console.warn('You clicked that button!'));
-
-  // USE WITH FIREBASE AUTH
-  // checkLoginStatus();
+const renderToDom = (divId, textToRender) => {
+  const selectedDiv = document.querySelector(divId);
+  selectedDiv.innerHTML = textToRender;
 };
 
-init();
+const renderForm = () => {
+  let domString = '<h1>FIND YOUR SONG LYRICS</H1>';
+  domString += `<div id="form" class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Artist</label>
+  <input type="email" class="form-control" id="artistInput" placeholder="Snoop Dogg">
+  <label for="exampleFormControlInput1" class="form-label">Song Title</label>
+  <input type="email" class="form-control" id="songInput" placeholder="Drop Like It's Hot">
+  <input id='submit' class="btn btn-primary" type="submit" value="Submit">
+  </div>`;
+  renderToDom('#form-container', domString);
+};
+
+const subEvent = () => {
+  const artistInput = document.getElementById('artistInput');
+  const songInput = document.getElementById('songInput');
+  document.querySelector('#form').addEventListener('submit', (e) => {
+    if (e.target.id === 'submit') {
+      console.error('button clicked');
+      const artist = artistInput.value;
+      const song = songInput.value;
+      getLyrics(artist, song).then((response) => {
+        renderToDom('#lyrics', response);
+      });
+    }
+  });
+};
+
+const startApp = () => {
+  renderForm();
+  subEvent();
+};
+
+startApp();
