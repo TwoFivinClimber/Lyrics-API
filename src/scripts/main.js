20f72823df335132680a4146f388c5ee22b6ae80
Brett Hughes <twofivinclimber@gmail.com>
2022-05-18 22:47:54 -0500

    function working

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,7 +5,7 @@ import 'bootstrap'; // import bootstrap elements and js
 import '../styles/main.scss';
 
 // API CALL
-const getLyrics = (artist, song) => ((resolve, reject) => {
+const getLyrics = (artist, song) => new Promise((resolve, reject) => {
   axios.get(`https://api.lyrics.ovh/v1/${artist}/${song}`)
     .then((response) => resolve(response.data))
     .catch((error) => reject(error));
@@ -18,33 +18,40 @@ const renderToDom = (divId, textToRender) => {
 
 const renderForm = () => {
   let domString = '<h1>FIND YOUR SONG LYRICS</H1>';
-  domString += `<div id="form" class="mb-3">
+  domString += `<form id="form" class="mb-3">
   <label for="exampleFormControlInput1" class="form-label">Artist</label>
-  <input type="email" class="form-control" id="artistInput" placeholder="Snoop Dogg">
+  <input type="text" class="inpFrm form-control" id="artistInput" placeholder="Snoop Dogg">
   <label for="exampleFormControlInput1" class="form-label">Song Title</label>
-  <input type="email" class="form-control" id="songInput" placeholder="Drop Like It's Hot">
-  <input id='submit' class="btn btn-primary" type="submit" value="Submit">
-  </div>`;
+  <input type="text" class="inpFrm form-control" id="songInput" placeholder="Drop Like It's Hot">
+  <div id='button-div'>
+  <input id='submit' class="subBtn btn btn-primary" type="submit" value="Submit">
+  </div>
+  </form>`;
   renderToDom('#form-container', domString);
 };
 
+const renderFooter = () => {
+  const domString = '<footer>Brought to you by lyrics.ovh and Brett Hughes</footer>';
+  renderToDom('#footer', domString);
+};
+
 const subEvent = () => {
-  const artistInput = document.getElementById('artistInput');
-  const songInput = document.getElementById('songInput');
   document.querySelector('#form').addEventListener('submit', (e) => {
-    if (e.target.id === 'submit') {
-      console.warn('button clicked');
-      const artist = artistInput.value;
-      const song = songInput.value;
-      getLyrics(artist, song).then((response) => {
-        renderToDom('#lyrics', response);
-      });
-    }
+    e.preventDefault();
+    const artist = document.querySelector('#artistInput').value;
+    const song = document.querySelector('#songInput').value;
+    getLyrics(artist, song).then((response) => {
+      let domString = `<h2>${artist}</h2><h5>"${song}"</h5>`;
+      domString += response.lyrics;
+      renderToDom('#lyrics', domString);
+    });
+    document.getElementById('form').reset();
   });
 };
 
 const startApp = () => {
   renderForm();
+  renderFooter();
   subEvent();
 };
 
